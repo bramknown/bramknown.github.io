57656a7461078b74914b40b38b93bf9710d77926
bramknown <111677005+bramknown@users.noreply.github.com>
2025-04-18 21:57:23 +1000

    Update model.js

--- dir/model.js
+++ dir/model.js
@@ -1,3 +1,4 @@
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 // Init
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
